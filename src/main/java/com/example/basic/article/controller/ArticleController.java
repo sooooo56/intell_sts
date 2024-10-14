72856f8133fc87088eb9f015266fd9c7254d5214
sooooo56 <sooooo4645@gmail.com>
2024-10-14 19:33:47 +0900

    로그아웃 구현

--- src/main/java/com/example/basic/article/controller/ArticleController.java
+++ src/main/java/com/example/basic/article/controller/ArticleController.java
@@ -2,6 +2,8 @@ package com.example.basic.article.controller;
 
 import com.example.basic.article.entity.Article;
 import com.example.basic.article.service.ArticleService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -44,12 +46,32 @@ public class ArticleController {
     }
 
     //목록
-    @GetMapping("/article/list")
-    public String list(Model model) {
+    @RequestMapping("/article/list")
+    public String list(Model model, HttpServletRequest request) {
         List<Article> articleList = articleService.getAll();
 
+        Cookie[] cookies = request.getCookies();
+        Cookie targetCookie = null;
+
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("loginUser")) {
+                    targetCookie = cookie;
+                }
+            }
+        }
+
+        // 단골이냐 아니냐(쿠폰 여부)
+        if(targetCookie == null) {
+            // loginUser 쿠폰 없으면 일반. (쿠폰이 없습니다 출력)
+            System.out.println("쿠키가 없습니다.");
+        } else {
+            // loginUser 쿠폰 있으면 단골. (loginUser 쿠폰값 출력)
+            System.out.println("loginedMember : " + targetCookie.getValue());
+            model.addAttribute("loginedUser", targetCookie.getValue());
+        }
+
         model.addAttribute("articleList", articleList);
-        model.addAttribute("loginedUser", "hong");
 
 
         return "article/list";
@@ -85,6 +107,8 @@ public class ArticleController {
         articleService.update(id, modifyForm.getTitle(), modifyForm.getBody());
         return "redirect:/article/detail/%d".formatted(id); // 브라우저 출력 => html 문자열로 출력
     }
+
+
 
 
 }
